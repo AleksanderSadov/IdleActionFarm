e82490ad72003a4ef17926f1f56ff9283d57cca6
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-29 14:17:28 +0700

    Add random crop shaking like under wind

--- Assets/Code/ScriptableObjects/CropConfig.cs
+++ Assets/Code/ScriptableObjects/CropConfig.cs
@@ -3,6 +3,10 @@
 [CreateAssetMenu(fileName = "CropConfig", menuName = "ScriptableObjects/CropConfig", order = 1)]
 public class CropConfig : ScriptableObject
 {
+    public Vector3 grownedStateScale;
+    public Vector3 slicedStateScale;
+    public Vector3 growingStateScale;
+
     [Tooltip("Time in seconds crop fully grows back again from gathered state")]
     public float fullGrowTime;
     [Tooltip("Delay in seconds crop displays visible small growing state after being gathered")]
@@ -12,7 +16,7 @@ public class CropConfig : ScriptableObject
     [Tooltip("Speed at which crop animation goes from small to full grown state")]
     public float animationFullGrowingSpeed;
 
-    public Vector3 grownedStateScale;
-    public Vector3 slicedStateScale;
-    public Vector3 growingStateScale;
+    public float windShakeSpeed = 3.0f;
+    public float windShakeMinimumAmplitude = 2.0f;
+    public float windShakeMaximumAmplitude = 4.0f;
 }
--- Assets/Code/Scripts/Gameplay/Crop.cs
+++ Assets/Code/Scripts/Gameplay/Crop.cs
@@ -15,11 +15,36 @@ public class Crop : MonoBehaviour
     public CropState state = CropState.Growned;
 
     private float timeGathered;
+    private Quaternion originalRotation;
+    private bool isShakingBackward = true;
+    private float windShakeMinimumAmplitude;
+    private float windShakeMaximumAmplitude;
+    private float windShakeRandomDelta;
+
+    private void Start()
+    {
+        originalRotation = transform.rotation;
+    }
 
     private void Update()
     {
         UpdateCropStateTransitions();
+        UpdateCropState();
         UpdateSizeBasedOnState();
+    }
+
+    public void GatherCrop()
+    {
+        switch (state)
+        {
+            case CropState.Growned:
+                state = CropState.Sliced;
+                break;
+            case CropState.Sliced:
+                state = CropState.Gathered;
+                timeGathered = Time.time;
+                break;
+        }
     }
 
     private void UpdateCropStateTransitions()
@@ -34,6 +59,21 @@ public class Crop : MonoBehaviour
                 {
                     state = CropState.Growned;
                 }
+                break;
+        }
+    }
+
+    private void UpdateCropState()
+    {
+        switch (state)
+        {
+            case CropState.Growned:
+                UpdateWindShakeDelta();
+                ShakeLikeWind();
+                break;
+            case CropState.Sliced:
+                UpdateWindShakeDelta();
+                ShakeLikeWind();
                 break;
         }
     }
@@ -73,17 +113,36 @@ public class Crop : MonoBehaviour
         }
     }
 
-    public void GatherCrop()
+    private void ShakeLikeWind()
     {
-        switch (state)
+        float deltaAngle = Mathf.DeltaAngle(transform.rotation.eulerAngles.z, originalRotation.eulerAngles.z);
+
+        if (Mathf.Abs(deltaAngle) >= windShakeRandomDelta)
         {
-            case CropState.Growned:
-                state = CropState.Sliced;
-                break;
-            case CropState.Sliced:
-                state = CropState.Gathered;
-                timeGathered = Time.time;
-                break;
+            isShakingBackward = !isShakingBackward;
+        }
+
+        if (isShakingBackward)
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * config.windShakeSpeed);
+        }
+        else
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * config.windShakeSpeed);
+        }
+    }
+
+    private void UpdateWindShakeDelta()
+    {
+        if (
+            windShakeMinimumAmplitude != config.windShakeMinimumAmplitude
+            || windShakeMaximumAmplitude != config.windShakeMaximumAmplitude
+        )
+        {
+            transform.rotation = originalRotation;
+            windShakeMinimumAmplitude = config.windShakeMinimumAmplitude;
+            windShakeMaximumAmplitude = config.windShakeMaximumAmplitude;
+            windShakeRandomDelta = Random.Range(windShakeMinimumAmplitude, windShakeMaximumAmplitude);
         }
     }
 }

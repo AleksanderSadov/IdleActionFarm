4072706e708056362c2de4729868e711d58bff8b
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-29 10:51:52 +0700

    Show scythe only when gathering

--- Assets/Code/Scripts/Gameplay/GatheringTool.cs
+++ Assets/Code/Scripts/Gameplay/GatheringTool.cs
@@ -6,6 +6,18 @@ public class GatheringTool : MonoBehaviour
     [SerializeField]
     private ThirdPersonController player;
 
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        meshRenderer.enabled = player.IsGathering();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Crop") || !player.IsGathering())

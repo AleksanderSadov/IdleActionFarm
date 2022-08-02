77a2bb3e265b4ec2d554b0330cf5c941aa0c11df
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-08-02 17:21:28 +0700

    Add smooth animation to stack UI

---
+++ Assets/Code/ScriptableObjects/UIConfig.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "UIConfig", menuName = "ScriptableObjects/UIConfig", order = 5)]
+public class UIConfig : ScriptableObject
+{
+    [Tooltip("Speed in seconds at which stack slider animates to new value")]
+    public float stackSliderSpeed = 1.0f;
+}
--- Assets/Code/Scripts/UI/CharacterStackSlider.cs
+++ Assets/Code/Scripts/UI/CharacterStackSlider.cs
@@ -1,19 +1,37 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterStackSlider : MonoBehaviour
 {
+    public UIConfig config;
     public CharacterPickupStack characterPickupStack;
 
     private Slider slider;
+    private float previousValue;
 
     private void Start()
     {
         slider = GetComponent<Slider>();
+        slider.value = 0;
+        previousValue = slider.value;
     }
 
     private void Update()
     {
-        slider.value = characterPickupStack.FillRatio;
+        float newValue = characterPickupStack.FillRatio;
+
+        if (newValue == previousValue)
+        {
+            return;
+        }
+
+        if (DOTween.IsTweening(slider))
+        {
+           DOTween.Kill(slider);
+        }
+
+        slider.DOValue(newValue, config.stackSliderSpeed);
+        previousValue = newValue;
     }
 }
